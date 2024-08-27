d1bb08e8e3fea854f57339f6f33e4596b2d3d526
b4l3i4 <lfperes155@gmail.com>
2024-08-27 10:41:07 -0300

    desafios_5_9

---
+++ desafio_5.py
@@ -0,0 +1,6 @@
+nun1 = int(input('Me fale o preimeiro número: '))
+nun2 = int(input('Me fale o segundo número: '))
+res = nun2/nun1
+print(' ________________________')
+print('| O resultado será: ',res,'|')
+print('|________________________|')
---
+++ desafio_9.py
@@ -0,0 +1,5 @@
+nun = int(input("Me diga um numero: "))
+resu = nun * 2
+print(' _____________________')
+print('| o resultado deu: ',resu,'|' )
+print('|_____________________|')
