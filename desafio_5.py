f44113b2ef6dd290f0bc1833d1031ec3b72bf4f8
b4l3i4 <lfperes155@gmail.com>
2024-08-27 10:46:01 -0300

    desafio_5_9

--- desafio_5.py
+++ desafio_5.py
@@ -1,6 +1,11 @@
+'''declarando as variaveis'''
 nun1 = int(input('Me fale o preimeiro número: '))
 nun2 = int(input('Me fale o segundo número: '))
+
+'''obtendo o resultado do desafio'''
 res = nun2/nun1
+
+'''formatando e imprimindo o resultado'''
 print(' ________________________')
 print('| O resultado será: ',res,'|')
 print('|________________________|')
--- desafio_9.py
+++ desafio_9.py
@@ -1,5 +1,10 @@
+'''declarando a variavel'''
 nun = int(input("Me diga um numero: "))
+
+'''mutiplicando para obter o objetivo''' 
 resu = nun * 2
+
+'''formatando e imprimindo o resultado'''
 print(' _____________________')
 print('| o resultado deu: ',resu,'|' )
 print('|_____________________|')
